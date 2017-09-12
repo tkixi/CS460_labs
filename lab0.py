1bffa564d08b0e098691f2a2d4eb09be5f47ed51
Tkixi <tkixi@users.noreply.github.com>
2017-09-12 14:24:41 -0400

    first commit

---
+++ lab0.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+db = mysql.connector.connect(user='root', host='127.0.0.1', password='Ilcppf1!', database='sakila')
+cursor = db.cursor()
+
+query = ('select film_id, title from film where film_id > 200;')
+
+cursor.execute(query)
+
+for info in cursor:
+	print( info)
+
+cursor.close()
+db.close()
+
+#Hat tip to Craig Einstein
